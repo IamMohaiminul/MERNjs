2f361bbc674a62e3e227918a989f5de30bfd81fb
Muhd Mohaiminul Islam <IamMohaiminul@Gmail.com>
2020-06-14 13:54:22 +0800

    split css from js bundle

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,35 @@ const path = require('path');
 const webpack = require('webpack');
 const DotENV = require('dotenv-webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
+
+let constants = {};
+if (process.env.NODE_ENV === 'production') {
+  constants = {
+    devtool: 'cheap-module-source-map',
+    output: {
+      filename: '[name].bundle.min.js',
+      sourceMapFilename: '[name].bundle.min.map',
+    },
+    MiniCssExtractPlugin: {
+      filename: '[name].bundle.min.css',
+      chunkFilename: '[id].bundle.min.css',
+    },
+  };
+} else {
+  constants = {
+    devtool: 'source-map',
+    output: {
+      filename: '[name].bundle.js',
+      sourceMapFilename: '[name].bundle.map',
+    },
+    MiniCssExtractPlugin: {
+      filename: '[name].bundle.css',
+      chunkFilename: '[id].bundle.css',
+    },
+  };
+}
 
 const pluginList = [
   new DotENV(),
@@ -12,6 +40,11 @@ const pluginList = [
     $: 'jquery',
     jQuery: 'jquery',
   }),
+  new MiniCssExtractPlugin({
+    filename: constants.MiniCssExtractPlugin.filename,
+    chunkFilename: constants.MiniCssExtractPlugin.chunkFilename,
+    ignoreOrder: false,
+  }),
 ];
 
 if (process.env.NODE_ENV === 'production') {
@@ -19,14 +52,15 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: constants.devtool,
   entry: {
     client: path.join(__dirname, 'client', 'index.jsx'),
     admin: path.join(__dirname, 'admin', 'index.jsx'),
   },
   output: {
     path: path.join(__dirname, 'public/dist'),
-    filename: '[name].bundle.js',
+    filename: constants.output.filename,
+    sourceMapFilename: constants.output.sourceMapFilename,
   },
   mode: process.env.NODE_ENV || 'development',
   resolve: {
@@ -41,7 +75,16 @@ module.exports = {
       },
       {
         test: /\.(css|scss)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
       },
       {
         test: /\.(jpg|jpeg|png|gif|mp3)$/,
