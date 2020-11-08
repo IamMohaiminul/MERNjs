2da92ecb441f0c9c1c056e0778d71a9ed17efc12
Muhd Mohaiminul Islam <IamMohaiminul@Gmail.com>
2020-11-08 12:59:28 +0800

    Update public dir

--- admin/index.jsx
+++ admin/index.jsx
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../public/stylesheets/lux.bootswatch.css';
+import '../public/styles/lux.bootswatch.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'toastr/build/toastr.min.css';
-import '../public/stylesheets/admin.css';
+import '../public/styles/admin.css';
 
 import './config';
 
